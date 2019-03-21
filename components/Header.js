827c30ab3d8a381fdb9d73f1a83e9c8d26904005
Athina Bekakou <athinabek@grnet.gr>
2019-03-21 18:09:49 +0200

    Improve text in terminal

--- components/Header.js
+++ components/Header.js
@@ -2,18 +2,17 @@ import Link from 'next/link'
 // https://jsfiddle.net/mattboldt/ovat9jmp/
 import Typed from 'typed.js'
 
-const linkStyle = {
-  marginRight: 15
-}
 
 class Header extends React.Component {
   componentDidMount() {
   	// If you want to pass more options as props, simply add
     // your desired props to this destructuring assignment.
     const strings  = [
-    	'Some <i>strings</i> are slanted',
-      'Some <strong>strings</strong> are bold',
-      'HTML characters &times; &copy;'
+    	// 'Some <i>strings</i> are slanted',
+      // 'Some <strong>strings</strong> are bold',
+      // 'HTML characters &times; &copy;'
+      'Welcome, my friend...',
+      'It is nice to see you again! :)',
     ];
     // You can pass other options here, such as typing speed, back speed, etc.
     const options = {
@@ -34,8 +33,7 @@ class Header extends React.Component {
   render() {
     return (
       <div>
-        <div className="wrap">
-          <h1>Typed.js</h1>
+        <div style={styles.terminal}>
           <div className="type-wrap">
             <span
             style={{ whiteSpace: 'pre' }}
@@ -48,17 +46,42 @@ class Header extends React.Component {
           <button onClick={() => this.typed.reset()}>Reset</button>
           <button onClick={() => this.typed.destroy()}>Destroy</button>
           </div>
-          <div>
+          <div style={styles.navigationMenu}>
             <Link href="/">
-              <a style={linkStyle}>Home</a>
+              <a style={styles.link}>Home</a>
             </Link>
             <Link href="/about">
-              <a style={linkStyle}>About</a>
+              <a style={styles.link}>About</a>
             </Link>
         </div>
       </div>
     );
   }
 }
-
+const styles = {
+  link: {
+    marginRight: 15,
+  },
+  navigationMenu: {
+    margin: 10,
+  },
+  terminal: {
+    margin: 'auto',
+    backgroundColor: 'black',
+    // color: '#4AF626',
+    color: 'white',
+    width: 600,
+    height: 300,
+    padding: 10,
+    fontFamily: 'Courier,Monaco,monospace',
+    // fontWeight: 'normal',
+    webkitFontSmoothing: 'antialiased',
+    mozOsxFontSmoothing: 'grayscale',
+    // display: 'flex',
+    // justifyContent: 'center',
+  }
+}
+const linkStyle = {
+  marginRight: 15
+};
 export default Header
